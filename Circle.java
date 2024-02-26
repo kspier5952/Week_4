0bf77c42aab89ae2af8d9b9631227807c92dcdda
kspier5952 <159089040+kspier5952@users.noreply.github.com>
2024-02-26 14:49:51 -0500

    made circle

---
+++ Circle.java
@@ -0,0 +1,10 @@
+public class Circle 
+{
+    public static void main(String[]args) {
+        double radius = 4.28;
+        double area = (3.14)*(radius)*(radius);
+        double circumference = 2*3.14*(radius);
+        System.out.println("The area of the circle is " + area);
+        System.out.println("The circumference of the circle is " + circumference);
+    }
+}
--- rectangle.java
+++ rectangle.java
@@ -1,7 +1,9 @@
-public class rectangle
-{
-    public static void main(String[]args)
-    {
+/**
+ * Katelyn Spier
+ * 2/26/24
+ */
+public class Rectangle {
+    public static void main(String[]args){
         System.out.println("***************");
         System.out.println("*             *");
         System.out.println("*             *");

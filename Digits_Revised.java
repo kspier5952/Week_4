c9873e1726feccfb6d0c16dd3782148df99e5ffc
kspier5952 <159089040+kspier5952@users.noreply.github.com>
2024-02-28 14:57:14 -0500

    Revised Digits

--- Digits_Revised.java
+++ Digits_Revised.java
@@ -1,16 +1,20 @@
+import java.util.*;
 /**
  * Katelyn Spier 
- * 2/27/24
+ * 2/28/24
  */
-public class Digits {
+public class Digits_Revised {
     public static void main(String[]args){
         int number;
-        number = 319;
         int hundreds;
-        hundreds = (number-(number%100))/100;
         int tens;
-        tens = ((number%100)-(number%10))/10;
         int ones;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a 3-digit number: ");
+        number = input.nextInt();
+        input.close();
+        hundreds = (number-(number%100))/100;
+        tens = ((number%100)-(number%10))/10;
         ones = (number%10);
         System.out.println("The hundreds place is " + hundreds);
         System.out.println("The tens place is " + tens);
